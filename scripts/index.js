64dedce8b9f70ed4f22ab5dafdb4cb3d928bf530
rittbys <rittbys@users.noreply.github.com>
2016-03-06 23:34:46 -0600

    Added some nice transitions

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@
 
   function init() {
     document.getElementById('preview-image').className = "shadow center raised";
+    document.getElementById('dl-btn').className = "top-btn shadow white";
+    document.getElementById('src-btn').className = "top-btn shadow white";
   };
 
   window.onload = function () {
