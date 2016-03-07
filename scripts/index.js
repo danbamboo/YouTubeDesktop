e26d540af2ef5cf5e95e05ac912526ade25c88bf
rittbys <rittbys@users.noreply.github.com>
2016-03-06 23:08:58 -0600

    Did a bit of work on the home page

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+(function () {
+
+  function init() {
+    document.getElementById('preview-image').className = "shadow center raised";
+  };
+
+  window.onload = function () {
+    init();
+  };
+})();
